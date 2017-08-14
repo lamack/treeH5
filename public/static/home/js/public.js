b4c49747b5e7d56e381589c120527aceafdefbad
lamack <shopex@administratordeMac-mini-2.local>
2017-08-14 16:51:00 +0800

    vv

--- public/static/home/js/public.js
+++ public/static/home/js/public.js
@@ -4,17 +4,17 @@ $('#back').on('click',function(){
 	window.history.back(-1)
 })
 //请求地址的域名
-var host = 'http://xxxx';
+var host = 'http://trees.com/index.php/index/index';
 var baseData = {
 
 }
 var getData=function(data){
-    type = data.type ? 'get':'type';
-    data = data.data? {}:data.data
+    type = data.type ? 'get':data.type;
+    var datas = data.data? {}:data.data
     $.ajax({
-    	type:type,
-    	url:host+data.url,
-    	data:$.extend(baseData,data),
-    	success:data.success
+        type:type,
+        url:host+data.url,
+        data:$.extend(baseData,datas),
+        success:data.success
     })
 }
