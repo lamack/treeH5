a6f75a8c667e1b15d7403bf07d6264006f8c7d0c
sunqiangqiang <sunqiangqiang@shopex.cn>
2017-08-17 16:47:10 +0800

    首页问题

--- application/database.php
+++ application/database.php
@@ -7,7 +7,7 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'dolphin',
+    'database'        => 'trees',
     // 用户名
     'username'        => 'root',
     // 密码
