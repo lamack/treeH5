12ef0da11c90284fbbe6b7e5aae4f594ed4c020c
zhouxiaogong <zhouxiaogong@shopex.cn>
2017-09-12 17:30:25 +0800

    task

--- vendor/taskphp/taskphp/tasks/syncdriver/syncdriverTask.php
+++ vendor/taskphp/taskphp/tasks/syncdriver/syncdriverTask.php
@@ -16,22 +16,33 @@ class syncdriverTask extends Task{
 
       Utils::dbConfig(Utils::config('DB','syncdriver'));
 
+      //dis
+      $sql3 = 'delete from game_driver_temp';
+      Utils::model("driver_temp")->execute($sql3);
+      $sql5 = 'delete from game_driver_total';
+      Utils::model("driver_total")->execute($sql5);
+      //update
+      $sql4 = 'insert into game_driver_temp (select b.* from game_driver_trip b left join game_member a on b.PHONE = a.contact  where  unix_timestamp(b.END_DATETIME)>a.passenger_time)';
+      Utils::model("driver_temp")->execute($sql4);
+      
+      //总计
+      $sql5 = 'insert into game_driver_total (MILES,PHONE) select sum(MILES) as MILES,PHONE from game_driver_temp where 1 group by PHONE';
+      Utils::model("driver_total")->execute($sql5);
+
 	    //取总量程
         $sql = 'update game_member a 
-inner join (select sum(MILES) as total,PHONE,CREATE_DATETIME from game_driver_trip WHERE TO_DAYS(CREATE_DATETIME)=TO_DAYS(NOW()) ) b on a.contact = b.PHONE  
-set a.driver_mileage = CASE b.total
-            WHEN b.total>40 THEN 40
-            WHEN b.total<40 THEN b.total
-        END,a.green_max = CASE b.total
-            WHEN b.total>40 THEN 40+a.green_max
-            WHEN b.total<40 THEN ceil(b.total)+a.green_max
-        END,a.green = CASE b.total
-            WHEN b.total>40 THEN 40+a.green
-            WHEN b.total<40 THEN ceil(b.total)+a.green
-        END,create_time = NOW() where TO_DAYS(a.create_time)<>TO_DAYS(NOW())';
+inner join  game_driver_total  b on a.contact = b.PHONE  
+set a.driver_mileage = b.MILES,a.green_max = ceil(b.MILES)+a.green_max,a.green = ceil(b.MILES)+a.green where 1';
+
+       
       
         Utils::model("member")->execute($sql);
-      
-		  flush();
+        
+        //同步时间
+        //update time
+        $sql1 = 'update game_member set driver_time = unix_timestamp() where 1';
+        Utils::model("member")->execute($sql1);
+        
+		flush();
 	}
 }
--- vendor/taskphp/taskphp/tasks/syncwxsport/syncwxsportTask.php
+++ vendor/taskphp/taskphp/tasks/syncwxsport/syncwxsportTask.php
@@ -19,28 +19,35 @@ class syncwxsportTask extends Task{
       //dis
       $sql3 = 'delete from game_wxsport_temp';
       Utils::model("wxsport_temp")->execute($sql3);
+      $sql5 = 'delete from game_wxsport_total';
+      Utils::model("wxsport_temp")->execute($sql5);
       //update
       $sql4 = 'insert into game_wxsport_temp (select b.* from game_wxsport b left join game_member a on b.LoginId = a.sign  where  unix_timestamp(b.updatetime)>a.create_time)';
       $res = Utils::model("wxsport_temp")->execute($sql4);
-      // if ($res) {
-      //   Utils::model("wxsport_temp")->addAll($res,array(),true);
-      // }
+      
+      //总计
+      $sql5 = 'insert into game_wxsport_total(Step,LoginId,AccuracyNum) select sum(Step) as steps,LoginId,sum(AccuracyNum) as nums from game_wxsport_temp where 1 ';
+      Utils::model("wxsport_total")->execute($sql5);
       
       //兑换成长币
       $develop = Utils::model("develop")->find();
 	    //取步数 步数换算绿值 答题数换算成长币
+//         $sql = 'update game_member a 
+// inner join (select Step,AccuracyNum,AddTime,LoginId,updatetime from game_wxsport_temp WHERE 1 ) b on a.sign = b.LoginId  
+// set a.steps = CASE b.Step
+//             WHEN b.Step>10000 THEN (10000+a.steps)
+//             ELSE (b.Step+a.steps)
+//         END,a.green_max = CASE b.Step
+//             WHEN b.Step>10000 THEN ceil(10000*0.6*2/1000)+a.green_max
+//             WHEN b.Step<10000 THEN ceil(b.Step*0.6*2/1000)+a.green_max
+//         END,a.green = CASE b.Step
+//             WHEN b.Step>10000 THEN ceil(10000*0.6*2/1000)+a.green
+//             WHEN b.Step<10000 THEN ceil(b.Step*0.6*2/1000)+a.green
+//         END,a.share = ceil(b.AccuracyNum*'.intval($develop['cash_share']).')+a.share where 1 ';
+
         $sql = 'update game_member a 
-inner join (select Step,AccuracyNum,AddTime,LoginId,updatetime from game_wxsport_temp WHERE 1 ) b on a.sign = b.LoginId  
-set a.steps = CASE b.Step
-            WHEN b.Step>10000 THEN (10000+a.steps)
-            ELSE (b.Step+a.steps)
-        END,a.green_max = CASE b.Step
-            WHEN b.Step>10000 THEN ceil(10000*0.6*2/1000)+a.green_max
-            WHEN b.Step<10000 THEN ceil(b.Step*0.6*2/1000)+a.green_max
-        END,a.green = CASE b.Step
-            WHEN b.Step>10000 THEN ceil(10000*0.6*2/1000)+a.green
-            WHEN b.Step<10000 THEN ceil(b.Step*0.6*2/1000)+a.green
-        END,a.share = ceil(b.AccuracyNum*'.intval($develop['cash_share']).')+a.share where 1 ';
+inner join  game_wxsport_total b on a.sign = b.LoginId  
+set a.steps = b.Step+a.steps,a.green_max = ceil(b.Step*0.6*2/1000)+a.green_max,a.green = ceil(b.Step*0.6*2/1000)+a.green,a.share = ceil(b.AccuracyNum*10)+a.share where 1 ';
 
         Utils::model("member")->execute($sql);
         
