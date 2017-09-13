6260f308af34c8ac6d7367469157f012cefec6ee
zhouxiaogong <zhouxiaogong@shopex.cn>
2017-09-13 21:49:22 +0800

    user

--- vendor/taskphp/taskphp/tasks/syncuserinfo/syncuserinfoTask.php
+++ vendor/taskphp/taskphp/tasks/syncuserinfo/syncuserinfoTask.php
@@ -18,7 +18,7 @@ class syncuserinfoTask extends Task{
       // $sql = 'replace into game_member (username,company,class,contact,sign,class_no,company_no,industry,industry_no) SELECT user_name,company,team_name,phone,uid,team_code,company,county,county FROM game_user_info a where not exists(select * from game_member where TO_DAYS(a.update_time) = TO_DAYS(NOW()) AND a.uid = game_member.sign )';
       $sql = 'replace into game_member (username,company,class,contact,sign,class_no,company_no,industry,industry_no) 
 SELECT user_name,company,team_name,phone,uid,team_code,company,county,county FROM game_user_info a 
-where not exists(select * from game_member where contact = a.phone ) AND TO_DAYS(a.create_time) = TO_DAYS(NOW()) ';
+where  TO_DAYS(a.create_time) = TO_DAYS(NOW()) AND not exists(select * from game_member where contact = a.phone )  ';
       Utils::model("member")->execute($sql);
       
 		  flush();
