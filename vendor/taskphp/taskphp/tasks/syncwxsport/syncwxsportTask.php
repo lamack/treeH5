d19f7f96e00d392d9b586910747e03b7e7a6a51a
zhouxiaogong <zhouxiaogong@shopex.cn>
2017-09-27 23:28:49 +0800

    wx

--- vendor/taskphp/taskphp/tasks/syncwxsport/syncwxsportTask.php
+++ vendor/taskphp/taskphp/tasks/syncwxsport/syncwxsportTask.php
@@ -22,7 +22,8 @@ class syncwxsportTask extends Task{
       $sql5 = 'delete from game_wxsport_total';
       Utils::model("wxsport_temp")->execute($sql5);
       //update
-      $sql4 = 'insert into game_wxsport_temp (select b.* from game_wxsport b left join game_member a on b.LoginId = a.sign  where  unix_timestamp(b.updatetime)>a.wxsport_time)';
+      // $sql4 = 'insert into game_wxsport_temp (select b.* from game_wxsport b left join game_member a on b.LoginId = a.sign  where  unix_timestamp(b.updatetime)>a.wxsport_time)';
+      $sql4 = 'insert into game_wxsport_temp (select b.* from game_wxsport b left join game_member a on b.LoginId = a.sign  where  1';
       $res = Utils::model("wxsport_temp")->execute($sql4);
       
       //总计
@@ -45,9 +46,12 @@ class syncwxsportTask extends Task{
 //             WHEN b.Step<10000 THEN ceil(b.Step*0.6*2/1000)+a.green
 //         END,a.share = ceil(b.AccuracyNum*'.intval($develop['cash_share']).')+a.share where 1 ';
 
+//         $sql = 'update game_member a 
+// inner join  game_wxsport_total b on a.sign = b.LoginId  
+// set a.steps = b.Step+a.steps,a.green_max = round(b.Step*0.7*3/1000)+a.green_max,a.green = round(b.Step*0.7*3/1000)+a.green,a.share = round(b.AccuracyNum*4)+a.share,wxsport_time=unix_timestamp(),a.accuracyNum = b.AccuracyNum+a.accuracyNum,a.total_time = b.UserTime+a.total_time where 1 ';
         $sql = 'update game_member a 
 inner join  game_wxsport_total b on a.sign = b.LoginId  
-set a.steps = b.Step+a.steps,a.green_max = round(b.Step*0.7*3/1000)+a.green_max,a.green = round(b.Step*0.7*3/1000)+a.green,a.share = round(b.AccuracyNum*4)+a.share,wxsport_time=unix_timestamp(),a.accuracyNum = b.AccuracyNum+a.accuracyNum,a.total_time = b.UserTime+a.total_time where 1 ';
+set a.steps = b.Step+a.steps,a.green_max = round(b.Step*0.7*3/1000)+a.green_max-a.last_green_sync,a.green = round(b.Step*0.7*3/1000)+a.green-a.last_green_sync,a.share = round(b.AccuracyNum*4),wxsport_time=unix_timestamp(),a.accuracyNum = b.AccuracyNum,a.total_time = b.UserTime,a.last_green_sync=round(b.AccuracyNum*4) where 1 ';
 
         Utils::model("member")->execute($sql);
         
